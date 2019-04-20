a5cecf47c6a621c9dee94c82bf889ba1cd58c32f
Vaibhawk <vaibhawk@IN-Vaibhawk.local>
2019-04-20 12:44:54 +0530

    Hover over on card wip

--- client/components/ProductCard.js
+++ client/components/ProductCard.js
@@ -11,7 +11,8 @@ const styles = theme => ({
         backgroundPosition: 'inherit'
     },
     card: {
-        cursor: "pointer"
+        cursor: "pointer",
+        position: "relative"
     },
     sellingPrice: {
         color: "deeppink"
@@ -23,21 +24,60 @@ const styles = theme => ({
     row: {
         display: "flex",
         flexDirection: "row"
+    },
+    actionContent: {
+        zIndex: 999,
+        top: "10%",
+        position: "absolute"
     }
 });
 
 class ProductCard extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            mouseOver: false
+        }
+    }
+
     render() {
         const p = this.props.product;
         const { classes } = this.props;
         return (
-            <Card className={classes.card}>
-                <CardMedia
-                    className={classes.media}
-                    image={`images/${p.image}`}
-                    title={p.name}
-                />
-                <CardContent>
+            <Card className={classes.card}
+                onMouseOver={
+                    e => {
+                        e.preventDefault();
+                        this.setState({ mouseOver: true })
+                    }
+                }
+                onMouseOut={
+                    e => {
+                        e.preventDefault();
+                        this.setState({ mouseOver: false })
+                    }
+                }
+            >
+                <CardContent style={this.state.mouseOver ? { opacity: 0.2 } : { opacity: 1 }}>
+                    <CardMedia
+                        className={classes.media}
+                        image={`images/${p.image}`}
+                        title={p.name}
+                    />
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom align="center" variant="subheading">
+                            {p.name}
+                        </Typography>
+                        <Typography gutterBottom align="center" className={classes.sellingPrice} variant="h6">
+                            <b>${p.discountedPrice == 0 ? p.price : p.discountedPrice}</b>
+                        </Typography>
+                        <Typography gutterBottom align="center" className={classes.originalPrice}>
+                            {p.discountedPrice == 0 ? <br /> : `$${p.price}`}
+                        </Typography>
+                    </CardContent>
+                </CardContent>
+                <CardContent hidden={!this.state.mouseOver} className={classes.actionContent}>
                     <Typography color="textSecondary" gutterBottom align="center" variant="subheading">
                         {p.name}
                     </Typography>
