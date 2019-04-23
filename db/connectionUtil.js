d0d6c2686387553cca87fc1b2becd51072003626
Vaibhawk <vaibhawk@IN-Vaibhawk.local>
2019-04-24 01:21:36 +0530

    Point to cleardb instance

--- db/connectionUtil.js
+++ db/connectionUtil.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 const pool = mysql.createPool({
     connectionLimit: 5,
-    host: 'localhost',
-    database: 'tshirtshop',
-    user: 'shopuser',
-    password: 'password'
+    host: process.env.CLEARDB_DATABASE_URL || 'localhost',
+    database: process.env.CLEARDB_SCHEMA || 'tshirtshop',
+    user: process.env.CLEARDB_USER || 'shopuser',
+    password: process.env.CLEARDB_PASSWORD || 'password'
 });
 
 module.exports = pool;
