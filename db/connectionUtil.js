005230f7c83bbdc3d31ca379e7bf583b32cf2ff7
Vaibhawk <vaibhawk@IN-Vaibhawk.local>
2019-04-24 01:40:29 +0530

    Fix

--- db/connectionUtil.js
+++ db/connectionUtil.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 const pool = mysql.createPool({
     connectionLimit: 5,
-    host: process.env.CLEARDB_DATABASE_URL || 'localhost',
+    host: process.env.CLEARDB_HOST || 'localhost',
     database: process.env.CLEARDB_SCHEMA || 'tshirtshop',
     user: process.env.CLEARDB_USER || 'shopuser',
     password: process.env.CLEARDB_PASSWORD || 'password'
